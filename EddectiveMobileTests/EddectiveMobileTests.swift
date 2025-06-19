301ef842fc8af9c690923250067c039013e0f0b8
Евгений <evgeniy@Evgenijs-MacBook-Pro.local>
2025-06-19 12:10:32 +0300

    create project

---
+++ EddectiveMobileTests/EddectiveMobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  EddectiveMobileTests.swift
+//  EddectiveMobileTests
+//
+//  Created by Евгений on 19.06.2025.
+//
+
+import Testing
+@testable import EddectiveMobile
+
+struct EddectiveMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
